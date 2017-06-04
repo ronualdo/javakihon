37943127165e9801eec4b798b4695ab7eebd66f6
Fernando <fernandoasfj@gmail.com>
2017-06-03 21:16:25 -0300

    Remove unused imports

--- src/main/java/kihon/home/HomeController.java
+++ src/main/java/kihon/home/HomeController.java
@@ -1,15 +1,9 @@
 package kihon.home;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.annotation.PostConstruct;
-import java.util.Arrays;
-import java.util.Optional;
 
 @Controller
 public class HomeController {
